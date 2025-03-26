6f2cb319b09f3186aaf77bf6b11788beccb455b0
Henry Tsai <henry88002605@gmail.com>
2025-03-26 21:15:45 +1000

    add routinng and navbar with styles and assets

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,29 @@ module.exports = function (api) {
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
         ],
+        plugins: [
+            "react-native-reanimated/plugin",
+            [
+                "module-resolver",
+                {
+                    root: ["./"],
+                    extensions: [
+                        ".ios.js",
+                        ".android.js",
+                        ".js",
+                        ".ts",
+                        ".tsx",
+                        ".json",
+                    ],
+                    alias: {
+                        // Define your aliases here
+                        "@components": "./src/components",
+                        "@screens": "./src/screens",
+                        "@utils": "./src/utils",
+                        // Add more aliases as needed
+                    },
+                },
+            ],
+        ],
     };
 };
