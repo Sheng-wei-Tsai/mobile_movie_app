ee53aff0488a9d71e38bdb00c12e4c37b93abb12
SHENG-WEI TSAI <henry88002605@gmail.com>
2025-04-05 09:20:13 +1000

    hidden the status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,26 @@
+import React from "react";
 import { Stack } from "expo-router";
 import './globals.css';
 import { TrendingProvider } from "@/services/TrendingContext";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <TrendingProvider>
-      <Stack>
-        <Stack.Screen
+    <>
+      <StatusBar hidden={true} />
+      <TrendingProvider>
+        <Stack>
+        
+          <Stack.Screen
           name="(tabs)"
           options={{ headerShown: false }}
           />
-        <Stack.Screen
+          <Stack.Screen
           name="movies/[id]"
           options={{ headerShown: false }}
         />
-      </Stack>
-    </TrendingProvider>
+        </Stack>
+      </TrendingProvider>
+    </>
   );
 }
