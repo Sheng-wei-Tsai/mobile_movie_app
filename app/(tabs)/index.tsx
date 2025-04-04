4ef0794f28b4ee2366e11b68c70a04f40365ecde
SHENG-WEI TSAI <henry88002605@gmail.com>
2025-04-05 08:41:48 +1000

    fixed the duplicate movie_id and app the trendingCard component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
+import React from 'react';
 import { Image, ScrollView,Text, View, ActivityIndicator, FlatList } from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import {useRouter} from "expo-router";
 import useFetch from "@/services/useFetch";
-import {fetchMovies} from "@/services/api";
+import {fetchMovies} from "@/services/api"
+import {getTrendingMovies} from "@/services/appwrite";
 import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {
+      data: trendingMovies,
+      loading: trendingLoading,
+      error: trendingError,
+
+  } = useFetch(getTrendingMovies);
 
   const {
       data: movies,
@@ -25,20 +35,39 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 10, minHeight: "100%"}}>
 
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-          {moviesLoading ? (
+          {moviesLoading || trendingLoading ? (
               <ActivityIndicator
                 size="large"
                 color="#0000ff"
                 className="mt-10 self-center"
-              />) : moviesError ? (
-                  <Text>Error: {moviesError?.message}</Text>
+              />) : moviesError || trendingError ? (
+                  <Text>Error: {moviesError?.message || trendingError?.message }</Text>
           ) : (
               <View className="flex-1 mt-5">
                   <SearchBar
                       onPress={() => router.push("/search")}
                       placeholder="Search for a movie"
                   />
+                  {trendingMovies && (
+                      <View className="mt-10">
+                          <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+                      </View>
+                  )}
                   <>
+
+                      <FlatList
+                          horizontal
+                          showsHorizontalScrollIndicator={false}
+                          ItemSeparatorComponent={() => <View className="w-4"/>}
+                          className="mb-4 mt-3"
+                          data={trendingMovies}
+                          renderItem={({item, index}) => (
+                              <TrendingCard movie={item} index={index} />
+                          )}
+                          keyExtractor={(item, index) => `trending-${item.movie_id}-${index}`}
+                      />
+
+
                       <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
                       <FlatList
                             className="mt-2 pb-32"
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -42,18 +42,18 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     }
 };
 
-// export const getTrendingMovies = async (): Promise<
-//     TrendingMovie[] | undefined
-// > => {
-//     try {
-//         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-//             Query.limit(5),
-//             Query.orderDesc("count"),
-//         ]);
-//
-//         return result.documents as unknown as TrendingMovie[];
-//     } catch (error) {
-//         console.error(error);
-//         return undefined;
-//     }
-// };
+export const getTrendingMovies = async (): Promise<
+    TrendingMovie[] | undefined
+> => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc("count"),
+        ]);
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
+};
